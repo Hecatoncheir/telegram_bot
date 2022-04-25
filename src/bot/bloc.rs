de11fe056721e65aeadec94c7d20ada4edf456bd
Vitaliy <vostrikov.vitaliy.mikhailovich@gmail.com>
2022-04-25 15:54:22 +0300

    Make it work.

--- src/bot/bloc.rs
+++ src/bot/bloc.rs
@@ -1,27 +1,28 @@
-use std::sync::{
-    mpsc,
-    mpsc::{Receiver, Sender},
-};
+use async_channel::{Receiver, Sender};
 use std::sync::{Arc, Mutex};
 
-use tokio::runtime::Runtime;
-
+use teloxide::dispatching::{Dispatcher, UpdateFilterExt};
+use teloxide::error_handlers::LoggingErrorHandler;
 use teloxide::payloads::SendMessageSetters;
+use teloxide::{dptree, respond};
 use teloxide_core::adaptors::AutoSend;
 use teloxide_core::requests::Requester;
-use teloxide_core::types::{ChatId, ReplyMarkup};
+use teloxide_core::types::{ChatId, Message, ReplyMarkup, Update};
 use teloxide_core::Bot;
+use tokio::task;
 
 pub trait BLoC<Event, State> {
-    fn get_controller(&self) -> Arc<Mutex<Sender<Event>>>;
-    fn get_stream(&self) -> Arc<Mutex<Receiver<State>>>;
+    fn get_controller(&self) -> Arc<Sender<Event>>;
+    fn get_stream(&self) -> Arc<Receiver<State>>;
 }
 
 #[derive(Clone)]
 pub struct BotBloc {
-    bot: Arc<Mutex<AutoSend<Bot>>>,
-    event_controller: Arc<Mutex<Sender<BotBlocEvent>>>,
-    state_stream: Arc<Mutex<Receiver<BotBlocState>>>,
+    bot: AutoSend<Bot>,
+    event_controller: Arc<Sender<BotBlocEvent>>,
+    event_stream: Arc<Mutex<Receiver<BotBlocEvent>>>,
+    state_controller: Arc<Sender<BotBlocState>>,
+    state_stream: Arc<Receiver<BotBlocState>>,
 }
 
 #[derive(Clone)]
@@ -39,69 +40,110 @@ pub enum BotBlocEvent {
 
 #[derive(Clone)]
 pub enum BotBlocState {
+    Update { chat_id: i64, text: String },
     TextToChatSendSuccessful { chat_id: i64, text: String },
 }
 
 impl BotBloc {
-    pub fn new(bot: Arc<Mutex<AutoSend<Bot>>>) -> BotBloc {
-        let (event_controller, event_stream) = mpsc::channel::<BotBlocEvent>();
-        let (state_controller, state_stream) = mpsc::channel::<BotBlocState>();
+    pub fn new(bot: AutoSend<Bot>) -> BotBloc {
+        let (event_controller, event_stream) = async_channel::unbounded::<BotBlocEvent>();
+        let (state_controller, state_stream) = async_channel::unbounded::<BotBlocState>();
 
-        let event_controller = Arc::new(Mutex::new(event_controller));
-        let event_stream = event_stream;
+        let event_controller = Arc::new(event_controller);
+        let event_stream = Arc::new(Mutex::new(event_stream));
 
-        let state_controller = state_controller;
-        let state_stream = Arc::new(Mutex::new(state_stream));
-
-        for event in event_stream.iter() {
-            let mut runtime = Runtime::new().expect("Unable to create the runtime");
-
-            runtime.spawn(async move {
-                tokio::spawn(async {}).await;
-
-                match event {
-                    BotBlocEvent::TextToChatSend { chat_id, text } => {
-                        let bot = bot.clone().lock().unwrap();
-
-                        bot.send_message(ChatId(chat_id), text.clone()).await;
-
-                        let state = BotBlocState::TextToChatSendSuccessful { chat_id, text };
-                        state_controller.send(state).unwrap();
-                    }
-                    BotBlocEvent::TextToChatSendWithMarkup {
-                        chat_id,
-                        text,
-                        markup,
-                    } => {
-                        let bot = bot.clone().lock().unwrap();
-
-                        bot.send_message(ChatId(chat_id), text.clone())
-                            .reply_markup(markup)
-                            .await
-                            .unwrap();
-
-                        let state = BotBlocState::TextToChatSendSuccessful { chat_id, text };
-
-                        state_controller.send(state).unwrap();
-                    }
-                }
-            });
-        }
+        let state_controller = Arc::new(state_controller);
+        let state_stream = Arc::new(state_stream);
 
         BotBloc {
             bot,
             event_controller,
+            event_stream,
+            state_controller,
             state_stream,
+        }
+    }
+
+    pub async fn run(&self) {
+        let bot = self.bot.clone();
+        let state_controller = self.state_controller.clone();
+
+        let handler =
+            Update::filter_message().branch(
+                dptree::filter(|msg: Message| msg.text().is_some()).endpoint(
+                    |msg: Message,
+                     bot: AutoSend<Bot>,
+                     state_controller: Arc<Sender<BotBlocState>>| async move {
+                        let state = BotBlocState::Update {
+                            chat_id: msg.chat.id.0,
+                            text: msg.text().unwrap().to_string(),
+                        };
+                        state_controller.send(state).await;
+
+                        bot.send_message(msg.chat.id, "This is a group chat.")
+                            .await?;
+
+                        respond(())
+                    },
+                ),
+            );
+
+        let dispatch_handler = task::spawn(async move {
+            Dispatcher::builder(bot.clone(), handler)
+                .dependencies(dptree::deps![bot.clone(), state_controller.clone()])
+                .default_handler(|upd| async move {
+                    println!("Unhandled update: {:?}", upd);
+                })
+                .error_handler(LoggingErrorHandler::with_custom_text(
+                    "An error has occurred in the dispatcher",
+                ))
+                .build()
+                .setup_ctrlc_handler()
+                .dispatch()
+                .await;
+        });
+
+        tokio::join!(self.subscribe_on_events(), dispatch_handler);
+    }
+
+    async fn subscribe_on_events(&self) {
+        let event_stream = self.event_stream.clone();
+        let event_stream = event_stream.lock().unwrap();
+        let state_controller = self.state_controller.clone();
+        let bot = self.bot.clone();
+
+        for event in event_stream.recv().await {
+            match event {
+                BotBlocEvent::TextToChatSend { chat_id, text } => {
+                    bot.send_message(ChatId(chat_id), text.clone()).await;
+
+                    let state = BotBlocState::TextToChatSendSuccessful { chat_id, text };
+                    state_controller.send(state).await;
+                }
+                BotBlocEvent::TextToChatSendWithMarkup {
+                    chat_id,
+                    text,
+                    markup,
+                } => {
+                    bot.send_message(ChatId(chat_id), text.clone())
+                        .reply_markup(markup)
+                        .await
+                        .unwrap();
+
+                    let state = BotBlocState::TextToChatSendSuccessful { chat_id, text };
+
+                    state_controller.send(state).await;
+                }
+            }
         }
     }
 }
 
 impl BLoC<BotBlocEvent, BotBlocState> for BotBloc {
-    fn get_controller(&self) -> Arc<Mutex<Sender<BotBlocEvent>>> {
+    fn get_controller(&self) -> Arc<Sender<BotBlocEvent>> {
         self.event_controller.clone()
     }
-
-    fn get_stream(&self) -> Arc<Mutex<Receiver<BotBlocState>>> {
+    fn get_stream(&self) -> Arc<Receiver<BotBlocState>> {
         self.state_stream.clone()
     }
 }
@@ -109,30 +151,40 @@ impl BLoC<BotBlocEvent, BotBlocState> for BotBloc {
 #[cfg(test)]
 mod bot_bloc_test {
     use crate::bot::bloc::{BLoC, BotBloc, BotBlocEvent, BotBlocState};
+    use std::borrow::BorrowMut;
     use std::sync::{Arc, Mutex};
     use teloxide_core::requests::RequesterExt;
+    use tokio::sync::mpsc;
+    use tokio::task;
 
-    #[test]
-    fn can_send_event_and_get_state() {
+    #[tokio::test]
+    async fn can_send_event_and_get_state() {
         let token = "";
         let bot = teloxide::Bot::new(token).auto_send();
-        let bot = Arc::new(Mutex::new(bot));
 
-        let bot = BotBloc::new(bot);
+        let bloc = BotBloc::new(bot);
+        let bloc_reference = Arc::new(bloc);
 
-        let event = BotBlocEvent::TextToChatSend {
-            chat_id: 0,
-            text: "Test message".to_string(),
-        };
+        let bloc_for_spawn = bloc_reference.clone();
 
-        bot.get_controller().lock().unwrap().send(event);
+        let bloc_for_run = bloc_reference.clone();
 
-        if let Some(state) = bot.get_stream().lock().unwrap().iter().next() {
-            match state {
-                BotBlocState::TextToChatSendSuccessful { chat_id, text } => {
-                    println!("{:?}", text);
+        tokio::spawn(async move {
+            while let Ok(state) = bloc_for_spawn.get_stream().recv().await {
+                match state {
+                    BotBlocState::Update { chat_id, text } => {
+                        let event = BotBlocEvent::TextToChatSend {
+                            chat_id,
+                            text: text.to_string(),
+                        };
+
+                        bloc_for_spawn.get_controller().send(event).await;
+                    }
+                    BotBlocState::TextToChatSendSuccessful { .. } => {}
                 }
             }
-        }
+        });
+
+        bloc_for_run.run().await;
     }
 }
